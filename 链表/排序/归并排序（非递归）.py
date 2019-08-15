407b75039445b50df0b2e0e1e5f2e6f740905d95
songsylvia <39432984+songsylvia@users.noreply.github.com>
2019-08-15 22:55:06 +0800

    Update 归并排序（非递归）.py

--- 链表/排序/归并排序（非递归）.py
+++ 链表/排序/归并排序（非递归）.py
@@ -1,9 +1,12 @@
 #148. Sort List  https://leetcode.com/problems/sort-list/
+class ListNode:
+    def __init__(self,x):
+        self.val = x
+        self.next = None
 class Solution:
-    def sortList(self, head: ListNode) -> ListNode:
-        if head is None or head.next is None:
+    def sortList(self,head:ListNode) -> ListNode:
+        if not head or not head.next:
             return head
-        ##求出链表长度
         dummy = ListNode(0)
         dummy.next = head
         cur = head
@@ -11,40 +14,49 @@ class Solution:
         while cur:
             cur = cur.next
             n +=1
-        #即长度 为n
+        ###总结点数量为n
         i = 1
         while (i<n):
             cur = dummy
             j = 0
-            #print("i的值",i,"cur的值",cur.val,cur)
-            while (j+i<n):
+            while (j+i)<n:
                 left = cur.next
                 right = cur.next
                 for k in range(i):
                     right = right.next
+                #左右分别放到了该放的位置
                 l,r = 0,0
-                while (l<i and r<i and right):
-                    if left.val <right.val:
+                while l<i and r<i and right:
+                    if left.val<right.val:
                         cur.next = left
                         cur = cur.next
                         left = left.next
-                        l+=1
+                        l += 1
                     else:
-                        cur.next = right 
+                        cur.next = right
                         cur = cur.next
                         right = right.next
                         r +=1
-                while l<i:
+                while l < i:
                     cur.next = left
                     cur = cur.next
                     left = left.next
                     l +=1
                 while r<i and right:
                     cur.next = right
-                    cur = right
+                    cur = cur.next
                     right = right.next
-                    r +=1
+                    r += 1
                 cur.next = right
-                j +=i*2
-            i *=2
+                j += i*2
+            i *= 2
         return dummy.next
+
+
+if __name__ == '__main__':
+    head = ListNode(5)
+    head.next = ListNode(4)
+    head.next.next = ListNode(11)
+    head.next.next.next = ListNode(0)
+    res = Solution().sortList(head)
+    print(res.val)
